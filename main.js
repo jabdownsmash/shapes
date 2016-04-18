7953c4964bc758188b85bcfa149d9d8e9f13fdb0
Aaron Santiago <aaronsantiago@gmail.com>
2016-04-17 21:54:49 -0400

    final ld version

--- main.js
+++ main.js
@@ -73,11 +73,61 @@ function init() {
 
     window.addEventListener( 'resize', onWindowResize, false );
     document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-    document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-    document.addEventListener("touchstart", reset2,false);
+    // document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+    // document.addEventListener("touchstart", reset2,false);
+    // document.addEventListener("touchend", function(){SELECTED = null;mousedown = false},false);
+    document.addEventListener('touchstart',function(e){ mapTouchEvents(e,'mousedown'); },false);
+document.addEventListener('touchmove',function(e){ mapTouchEvents(e,'mousemove'); },false);
+document.addEventListener('touchend',function(e){ mapTouchEvents(e,'mouseup'); },false);
+document.addEventListener('touchcancel',function(e){ mapTouchEvents(e,'mouseup'); },false);
     document.addEventListener("mousedown", reset,false);
-    document.addEventListener("touchend", function(){SELECTED = null;mousedown = false},false);
-    document.addEventListener("mouseup", function(){SELECTED = null;mousedown = false},false);
+    document.addEventListener("mouseup", function(){
+    if(letGoAlready)
+    {
+        if(INTERSECTED.clickFunc)
+        {
+            INTERSECTED.clickFunc();
+        }
+    }
+    else
+    {
+        letGoAlready = true;
+    }
+    SELECTED = null;mousedown = false},false);
+}
+function mapTouchEvents(event,simulatedType) {
+    
+    //Ignore any mapping if more than 1 fingers touching 
+    if(event.changedTouches.length>1){return;}
+    
+    var touch = event.changedTouches[0];
+    
+    //--https://developer.mozilla.org/en/DOM/document.createEvent--
+    eventToSimulate = document.createEvent('MouseEvent');
+    
+    //--https://developer.mozilla.org/en-US/docs/Web/API/event.initMouseEvent--
+    eventToSimulate.initMouseEvent(
+        simulatedType,      //type
+        true,               //bubbles
+        true,               //cancelable
+        window,             //view
+        1,                  //detail
+        touch.screenX,      //screenX
+        touch.screenY,      //screenY
+        touch.clientX,      //clientX
+        touch.clientY,      //clientY
+        false,              //ctrlKey
+        false,              //altKey
+        false,              //shiftKey
+        false,              //metaKey
+        0,                  //button
+        null                //relatedTarget
+    );
+
+    touch.target.dispatchEvent(eventToSimulate);
+    //This ignores the default scroll behavior
+    event.preventDefault();             
+    
 }
 function reset2(event){
     event.preventDefault();
@@ -281,24 +331,28 @@ function animate(timestamp) {
             triggerMenu(2);
             mousedown = false;
             SELECTED = null;
+            letGoAlready = false;
         }
         else if(SELECTED.position.x > distanceThreshold)
         {
             triggerMenu(0);
             mousedown = false;
             SELECTED = null;
+            letGoAlready = false;
         }
         else if(SELECTED.position.y < -distanceThreshold) ///down
         {
             triggerMenu(3);
             mousedown = false;
             SELECTED = null;
+            letGoAlready = false;
         }
         else if(SELECTED.position.y > distanceThreshold)
         {
             triggerMenu(1);
             mousedown = false;
             SELECTED = null;
+            letGoAlready = false;
         }
     }
     var intersects = raycaster.intersectObjects( scene.children );
@@ -355,9 +409,5 @@ function reset()
     if(!menuOn)
     {
         mousedown = true;
-    }
-    if(INTERSECTED.clickFunc)
-    {
-        INTERSECTED.clickFunc();
     }
 }
